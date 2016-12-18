fbf73a67434d34a8b53898af39385e8a1fde270f
nroi <nroi@mailbox.org>
2016-12-18 22:47:37 +0100

    Rename project

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Cpc.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cpc,
+    [app: :cpcache,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
