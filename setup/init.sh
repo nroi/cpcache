6a872489b9ec4256d24e6a77f02ddc84e786565e
nroi <nroi@mailbox.org>
2017-01-01 16:17:55 +0100

    Add config file and vm.args for releases

--- setup/init.sh
+++ setup/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ELIXIR_ERL_OPTIONS="-sname cpcache -mnesia dir '/var/cache/cpcache/mnesia'" /usr/bin/elixir create_mnesia_db.exs
+ELIXIR_ERL_OPTIONS="-name cpcache@127.0.0.1 -mnesia dir '/var/cache/cpcache/mnesia'" /usr/bin/elixir create_mnesia_db.exs
