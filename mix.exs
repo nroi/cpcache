a298d1686b8fa7eac0d06a1333d965e2fdc78b5f
nroi <nroi@mailbox.org>
2017-04-21 22:20:19 +0200

    Add :tomlex to applications in Mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Cpc.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :inets, :ssl, :ibrowse, :mnesia],
+    [applications: [:logger, :inets, :ssl, :ibrowse, :mnesia, :tomlex],
      mod: {Cpc, []}]
   end
 
