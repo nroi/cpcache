79620bb3aa801b34c72a32b102956b0b3a4b6998
nroi <nroi@mailbox.org>
2018-10-19 18:00:02 +0200

    Bump Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cpc.Mixfile do
     [
       app: :cpcache,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
