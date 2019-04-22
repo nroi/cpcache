54e6985fe92d01801ce832e40154fbe5a8552fbf
nroi <nroi@mailbox.org>
2019-04-22 18:11:26 +0200

    Add failing test case
    See issue #13

--- cpcache/test/cpc_test.exs
+++ cpcache/test/cpc_test.exs
@@ -31,23 +31,46 @@ defmodule CpcTest do
 
   def sha256_digest(binary), do: Base.encode16(:crypto.hash(:sha256, binary))
 
-  defp url_from_filename(repo, filename), do: "http://localhost:#{@port}/#{repo}/os/x86_64/#{filename}"
+  defp url_from_filename(repo, filename),
+    do: "http://localhost:#{@port}/#{repo}/os/x86_64/#{filename}"
 
   def get_sorted_files(repo) do
     get_db_file_body(repo)
     |> extract_tar_from_binary
-    |> Enum.map(fn {file, content} ->
-      {file, Cpc.AlpmUtils.parse_db(content)}
+    |> Enum.reduce([], fn {file, content}, acc ->
+      case Cpc.AlpmUtils.parse_db(content) do
+        {:ok, result} -> [{file, repo, result} | acc]
+        {:error, _} -> acc
+      end
     end)
-    |> Enum.sort_by(fn {_file, content} ->
+    |> Enum.sort_by(fn {_file, _repo, content} ->
       content["CSIZE"]
     end)
+  end
+
+  def get_sorted_files() do
+    ["core", "extra", "community"]
+    |> Enum.map(&get_sorted_files/1)
+    |> Enum.concat()
   end
 
   def download_and_digest(url) do
     {:ok, 200, _headers, ref} = :hackney.get(url)
     {:ok, body} = :hackney.body(ref)
     sha256_digest(body)
+  end
+
+  def download_and_abort(repo, filename, abort_after_millisecs) do
+    url = url_from_filename(repo, filename)
+
+    task =
+      Task.async(fn ->
+        {:ok, 200, _headers, ref} = :hackney.get(url)
+        {:ok, _body} = :hackney.body(ref)
+      end)
+
+    :timer.sleep(abort_after_millisecs)
+    Task.shutdown(task, :brutal_kill)
   end
 
   def test_file(repo, filename, digest_should) do
@@ -67,10 +90,50 @@ defmodule CpcTest do
   test "GET the smallest file" do
     repo = "core"
     sorted_files = get_sorted_files(repo)
-    [{_filename, filemap} | _] = sorted_files
+    [{_filename, _repo, filemap} | _] = sorted_files
     filename = filemap["FILENAME"]
     digest_should = String.upcase(filemap["SHA256SUM"])
     test_file(repo, filename, digest_should)
   end
 
+  test "a client aborting the download does not cause errors for other clients fetching the same file" do
+    sorted_files = get_sorted_files()
+
+    # with the speed throttled to 100KiB/s, a file with a size somewhere around 300KiB should be sufficient.
+    range = trunc(307_200 * 0.8)..trunc(307_200 * 1.2)
+
+    {_filename, repo, filemap} =
+      Enum.find(sorted_files, fn
+        {_filename, repo, filemap} -> String.to_integer(filemap["CSIZE"]) in range
+      end)
+
+    filename = filemap["FILENAME"]
+    digest_should = String.upcase(filemap["SHA256SUM"])
+    url = url_from_filename(repo, filename)
+
+    task1 =
+      Task.async(fn ->
+        {:ok, 200, _headers, ref} = :hackney.get(url)
+        {:ok, _body} = :hackney.body(ref)
+      end)
+
+    :timer.sleep(300)
+
+    task2 =
+      Task.async(fn ->
+        {:ok, 200, _headers, ref} = :hackney.get(url)
+        {:ok, body} = :hackney.body(ref)
+        body
+      end)
+
+    :timer.sleep(300)
+    Task.shutdown(task1, :brutal_kill)
+
+    # task1 has been killed, but this must not interfere with task2! cpcache needs to be able to recognize
+    # that there is still another ongoing download for the same file and therefore it must not close the
+    # connection to the remote mirror.
+    body = Task.await(task2)
+    digest_is = sha256_digest(body)
+    assert digest_is == digest_should
+  end
 end
--- cpcache/test/support/alpm_utils.ex
+++ cpcache/test/support/alpm_utils.ex
@@ -1,13 +1,23 @@
 defmodule Cpc.AlpmUtils do
+  require Logger
+
   def parse_db(db) do
-    db
-    |> String.replace_trailing("\n", "")
-    |> String.split("\n\n")
-    |> Enum.reduce([], fn
-      "%" <> rest, acc ->
-        [header, value] = String.split(rest, "\n", parts: 2)
-        [{String.replace_suffix(header, "%", ""), value} | acc]
-    end)
-    |> Map.new()
+    # try-rescue is used because experience has shown that the db files sometimes contain borked description files.
+    try do
+      result =
+        db
+        |> String.replace_trailing("\n", "")
+        |> String.split("\n\n")
+        |> Enum.reduce([], fn
+          "%" <> rest, acc ->
+            [header, value] = String.split(rest, "\n", parts: 2)
+            [{String.replace_suffix(header, "%", ""), value} | acc]
+        end)
+        |> Map.new()
+
+      {:ok, result}
+    rescue
+      e -> {:error, e}
+    end
   end
 end
