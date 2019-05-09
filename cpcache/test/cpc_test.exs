1bdb75d667bbdddf88269747d5af9c6c631be75d
nroi <nroi@mailbox.org>
2019-05-09 11:43:18 +0200

    Add test case, include latency measurements
    Average latency: 108390.4
    Median latency: 69945
    All latencies: [28640, 139042, 59896, 73408, 63154, 56721, 66543, 59903, 63222, 63330, 69907, 66632, 69945, 153324, 189456, 194030, 184838, 192023, 187781, 186013]

--- cpcache/test/cpc_test.exs
+++ cpcache/test/cpc_test.exs
@@ -34,7 +34,7 @@ defmodule CpcTest do
   defp url_from_filename(repo, filename),
     do: "http://localhost:#{@port}/#{repo}/os/x86_64/#{filename}"
 
-  def get_sorted_files(repo) do
+  def get_files_sorted_by_size(repo) do
     get_db_file_body(repo)
     |> extract_tar_from_binary
     |> Enum.reduce([], fn {file, content}, acc ->
@@ -44,13 +44,13 @@ defmodule CpcTest do
       end
     end)
     |> Enum.sort_by(fn {_file, _repo, content} ->
-      content["CSIZE"]
+      String.to_integer(content["CSIZE"])
     end)
   end
 
-  def get_sorted_files() do
+  def get_files_sorted_by_size() do
     ["core", "extra", "community"]
-    |> Enum.map(&get_sorted_files/1)
+    |> Enum.map(&get_files_sorted_by_size/1)
     |> Enum.concat()
   end
 
@@ -89,21 +89,41 @@ defmodule CpcTest do
 
   test "GET the smallest file" do
     repo = "core"
-    sorted_files = get_sorted_files(repo)
-    [{_filename, _repo, filemap} | _] = sorted_files
+    files_sorted_by_size = get_files_sorted_by_size(repo)
+    [{_filename, ^repo, filemap} | _] = files_sorted_by_size
     filename = filemap["FILENAME"]
     digest_should = String.upcase(filemap["SHA256SUM"])
     test_file(repo, filename, digest_should)
   end
 
+  test "GET the 20 smallest files" do
+    repo = "core"
+    # Skip the first file: It is already in cache, and we want to test the download behavior.
+    [_already_downloaded | files_sorted_by_size] = get_files_sorted_by_size(repo)
+
+    latencies = for {_filename, ^repo, filemap} <- Enum.take(files_sorted_by_size, 20) do
+      filename = filemap["FILENAME"]
+      url = url_from_filename(repo, filename)
+      digest_should = String.upcase(filemap["SHA256SUM"])
+      {microsecs, digest_is} = :timer.tc(__MODULE__, :download_and_digest, [url])
+      Logger.debug("Time required to download #{filename}: #{microsecs / 1000} ms")
+      assert digest_is == digest_should
+      microsecs
+    end
+    sorted_latencies = Enum.sort(latencies)
+    Logger.error("Average latency: #{Enum.sum(sorted_latencies) / Enum.count(sorted_latencies)}")
+    Logger.error("Median latency: #{Enum.at(sorted_latencies, 10)}")
+    Logger.error("All latencies: #{inspect latencies}")
+  end
+
   test "a client aborting the download does not cause errors for other clients fetching the same file" do
-    sorted_files = get_sorted_files()
+    files_sorted_by_size = get_files_sorted_by_size()
 
     # with the speed throttled to 100KiB/s, a file with a size somewhere around 300KiB should be sufficient.
     range = trunc(307_200 * 0.8)..trunc(307_200 * 1.2)
 
     {_filename, repo, filemap} =
-      Enum.find(sorted_files, fn
+      Enum.find(files_sorted_by_size, fn
         {_filename, _repo, filemap} -> String.to_integer(filemap["CSIZE"]) in range
       end)
 
