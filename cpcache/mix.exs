6975181b5ae614acf097a2f6c977ff4c7701bddf
nroi <nroi@mailbox.org>
2019-09-14 11:35:35 +0200

    Bump eyepatch version

--- cpcache/mix.exs
+++ cpcache/mix.exs
@@ -28,7 +28,7 @@ defmodule Cpc.Mixfile do
       {:hackney, "~> 1.15"},
       {:toml, "~> 0.5.2"},
       {:jason, "~> 1.1"},
-      {:eyepatch, git: "https://github.com/nroi/eyepatch.git", tag: "v0.1.11"},
+      {:eyepatch, git: "https://github.com/nroi/eyepatch.git", tag: "v0.1.12"},
       {:morbo, git: "https://github.com/nroi/morbo.git", tag: "0.1.3"}
     ]
   end
